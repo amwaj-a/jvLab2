db77c3d8a5d8f9d33f7b84dae71d83cbce74d3ec
Amwaj <amwaj.1431@outlook.com>
2024-05-22 18:27:13 +0300

    Buzz and Fuzz

--- lab2.js
+++ lab2.js
@@ -18,3 +18,16 @@ console.log(`sub ${sub(4, 2)}`);
 console.log(`mult ${mul(4, 2)}`);
 
 console.log(`div ${div(4, 2)}`);
+
+function naame(x) {
+  if (x % 3 == 0 && x % 5 == 0) {
+    console.log(`${x} is Buzz and Fuzz`);
+  } else if (x % 3 == 0) {
+    console.log(`${x} is Buzz`);
+  } else if (x % 5 == 0) {
+    console.log(`${x} is Fuzz`);
+    {
+    }
+  } else console.log("N");
+}
+naame(20);
